106be40e6cd6f4a8549cdbb0a990dfcf165ff3c2
namycodes <namycodes@yahoo.com>
2024-04-17 23:46:12 +0200

    Installed necessary dependacies

---
+++ navigators/stackNavigator.js
@@ -0,0 +1,11 @@
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+function RootStackNavigator() {
+	<Stack.Navigator>
+		{/* <Stack.Screen name="home" component={}/> */}
+	</Stack.Navigator>;
+}
+
+export default RootStackNavigator;
